b6fb80c3b66167f5678f567fcce815d4baea7d2d
Nate Brunette <n@tebru.net>
2022-09-29 12:25:49 -0500

    updating to v0.2.0

--- Formula/docat.rb
+++ Formula/docat.rb
@@ -1,14 +1,14 @@
 class Docat < Formula
   desc "Docker Compose orchestration CLI tool"
   homepage "https://github.com/docat-cli/docat"
-  version "0.1.0"
+  version "0.2.0"
 
   if Hardware::CPU.intel?
     url "https://github.com/docat-cli/docat/releases/download/v#{version}/docat-mac-x86_64.tar.gz"
-    sha256 "e4d9565a699cf86eefa69744fffd4b2ec2ca711495d4e524673e8a1e79678e1a"
+    sha256 "3bd022d75adaef0f0c8bb7fd3040e27ca7d2c3d97253e6a5397412c6c23a189d"
   else
-    url "https://github.com/docat-cli/docat/releases/download/v#{version}/docat-mac-aarch64.tar.gz"
-    sha256 "92088377a16399d89c94cf6ece74a3b6450fe9a55945f93e143a03be489aef63"
+    url "https://github.com/docat-cli/docat/releases/download/v#{version}/docat-mac-arm64.tar.gz"
+    sha256 "2fe253e21dda8a098306bb1992cc6c9827d30f417756cc4b42a65e92e91fcdab"
   end
 
   def install
